34a0e64294cb938ac6789ab6a8840df1ab7c50f1
wongakim-99 <202021000@sangmyung.kr>
2024-12-12 14:44:46 +0900

    feat : "웹서버 기말과제 초기세팅"

---
+++ smu_map/src/main/java/com/example/smu_map/SmuMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.smu_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmuMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmuMapApplication.class, args);
+	}
+
+}
---
+++ smu_map/src/test/java/com/example/smu_map/SmuMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smu_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmuMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
